39813e0815c016b9cd7c51d54e117c9e9a5183e9
Daniyal Bokhari <63890998+daniyalb@users.noreply.github.com>
2023-02-19 22:20:18 -0500

    added text prompt to sign-in or sign-up

--- main.py
+++ main.py
@@ -1,49 +1,65 @@
-# from pymongo import MongoClient
+from pymongo import MongoClient
 
 global current_user
 # Press the green button in the gutter to run the script.
-def does_user_exist(username):
+
+
+def does_user_exist(username) -> bool:
     # TODO implement this function
     pass
+
 
 def register_user(username, password):
     # TODO implement this function
     pass
 
+
 def signup():
     username = input("Please enter your username:")
     user_exists = does_user_exist(username)
     while does_user_exist(username):
-        username = input("Sorry! This user already exists\nPlease enter your username:")
+        username = input("Sorry! This user already exists\nPlease enter your "
+                         "username:")
     password = input("Please enter your password:")
 
     register_user(username, password)
     print("User registered successfully!")
 
+
 def fetch_user(username):
     # TODO fetch the user from the database
     pass
+
 
 def signin():
     username = input("Please enter your username:")
     user = fetch_user(username)
     while not user:
-        username = input("Sorry! This user does not exist\nPlease enter your username:")
+        username = input("Sorry! This user does not exist\nPlease enter your "
+                         "username:")
         user = fetch_user(username)
     password = input(f"Please enter the password for user {username}")
     while password != user["password"]:
-        password = input(f"Password incorrect! Please enter the password for user {username}")
+        password = input(f"Password incorrect! Please enter the password for "
+                         f"user {username}")
     global current_user
     current_user = user
     print(f"Welcome {username}!")
+
 
 def deposit():
     # TODO
     pass
 
+
+def withdraw():
+    # TODO
+    pass
+
+
 user_functions = {
     "deposit": deposit,
-
+    "withdraw": withdraw
 }
 if __name__ == '__main__':
     print("""
@@ -56,6 +72,14 @@ if __name__ == '__main__':
           __/ |                                                
          |___/                                                 
     """)
-    print("Welcome my cool bank, you can make your account an manage your money!")
+    print("Welcome my cool bank, you can make your account and manage your "
+          "money!")
     user_inp = ""
     while user_inp != 'exit':
+        option = ''
+        while option.lower() != 'si' or option.lower() != 'su':
+            option = input("Would you like to sign in (SI) or sign-up (SU)?: ")
+        if option.lower() == 'si':
+            signin()
+        else:
+            signup()
